22182e6e74dc455ad92892462b38bd0e06bc65bf
EdgardoBryan <edgardobryanescamilla@gmail.com>
2022-06-29 21:58:08 -0400

    added characters to page

--- client/src/components/CharacterPage.js
+++ client/src/components/CharacterPage.js
@@ -24,11 +24,13 @@ useEffect(()=>{
 getPeople()
 },[])
 
-  console.log("people", peopleData)
+  const getCharacters = peopleData.map((people,i)=>{
+    return <CharacterTiles key={i} name={people.name} people={people}/>
+  }) 
     return(
-        <div>
+        <div className="main-container">
             <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/maxresdefault-13.jpg?auto=format&q=60&fit=max&w=930" alt="star-wars-logo"/>
-        
+            <div className="container-class">{getCharacters}</div>
         </div>   
     )
 }
